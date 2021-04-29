71bdacd528d88456faff9622640f552943f00a74
Jose Guilherme <joseguilhermepimenteldarocha@gmail.com>
2021-04-29 14:46:50 -0300

    atualização

--- js/index.js
+++ js/index.js
@@ -28,16 +28,4 @@ $(function () {
     $($('li', '.social-class')[Math.floor($('li', '.social-class').length * Math.random())]).mouseenter();
 });
 
-window.onload = function () {
-    document.querySelector('.cont_modal').className = "cont_modal";
-}
-var c = 0;
-function open_close() {
-    if (c % 2 == 0) {
-        document.querySelector('.cont_modal').className = "cont_modal cont_modal_active";
-        c++;
-    } else {
-        document.querySelector('.cont_modal').className = "cont_modal";
-        c++;
-    }
-}
+/* cards */
